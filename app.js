2d9c3ac2607c6088aaa38d58159af13be0b86c83
Fadhlurrahman <kerenjugague@gmail.com>
2024-03-20 22:56:15 +0800

    fixing bugs and refactoring

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
-const express = require("express");
-const app = express();
-const fs = require("fs");
-const readExcel = require('read-excel-file/node');
+const readExcel = require("read-excel-file/node");
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
-const { Client, Location, List, Buttons, LocalAuth, qrcode, MessageMedia } = require('whatsapp-web.js');
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
 
+client.on("loading_screen", (percent, message) => {
+    console.log("LOADING SCREEN", percent, message);
+});
 
-app.get("/", (req, res) => {
+client.on("authenticated", () => {
+    console.log("AUTHENTICATED");
+});
 
-    const client = new Client({
-        authStrategy: new LocalAuth(),
-        puppeteer: { headless: false }
+client.on("ready", () => {
+    console.log("Client is ready!");
+
+    readExcel("./public/blast.xlsx").then(async (data) => {
+        data.shift();
+
+        for (i in data) {
+            const name = data[i][0];
+            const phone = data[i][1];
+
+            const msg = "Selamat malam";
+            const chatId = phone.substring(1) + "@c.us";
+
+            const timeout = i * 1000;
+
+            setTimeout(() => {
+                client.sendMessage(chatId, msg);
+                console.log(`Message Sent to Client ${name}`);
+            }, timeout);
+        }
     });
+});
 
-    client.initialize();
+client.on("qr", (qr) => {
+    qrcode.generate(qr, { small: true });
+});
 
-    client.on('loading_screen', (percent, message) => {
-        console.log('LOADING SCREEN', percent, message);
-    });
-
-
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
-
-    });
-
-    client.on('authenticated', () => {
-        console.log('AUTHENTICATED');
-    });
-
-    client.on('auth_failure', msg => {
-        // Fired if session restore was unsuccessful
-        console.error('AUTHENTICATION FAILURE', msg);
-    });
-
-
-    client.on('ready', async () => {
-
-
-        readExcel('./mobileData.xlsx').then(async (data) => {
-            for (i in data) {
-
-                let caption = "Hello Sir";
-                let number = data[i][0];
-                const chatId = number.substring(1) + "@c.us";
-
-                const media = MessageMedia.fromFilePath('./img.png');
-
-                client.sendMessage(chatId, media, { caption: caption });
-
-                console.log(`Messages Sent to Clients ${data[i][0]}`);
-            }
-        })
-    });
-    res.send("Please Check Your Console");
-})
-
-
-
-
-
-app.listen(1111, () => {
-    console.log("Please Open 👇 Server is Running");
-    console.log("http://localhost:1111");
-})
+client.initialize();
